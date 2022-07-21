fdd56b3f275f8f6df907b6a2cc38ef2cbd6eb67c
adildulobu <adildulobu@gmail.com>
2022-07-21 10:52:44 +0200

    Created Register user component

---
+++ src/screens/SignOut.tsx
@@ -0,0 +1,90 @@
+import { useState } from 'react';
+import { Alert } from 'react-native';
+import auth from '@react-native-firebase/auth';
+import { VStack, Heading, Icon, useTheme, HStack, IconButton } from 'native-base';
+import { CaretLeft, Envelope, Key } from 'phosphor-react-native';
+
+import Logo from '../assets/logo_primary.svg';
+
+import { Input } from '../components/Input';
+import { Button } from '../components/Button';
+
+export function SignOut() {
+  const [isLoading,setIsLoading] = useState(false); 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const { colors } = useTheme();
+
+  function handleSignOut() {
+    if(!email || !password || !confirmPassword) {
+      return Alert.alert('Registar', 'Preencha os campos');
+    }
+
+    setIsLoading(true);
+
+    auth().createUserWithEmailAndPassword(email,password)
+      .catch((error) =>{  
+        setIsLoading(false);
+
+        if(error.code === 'auth/invalid-email'){
+          return Alert.alert('Registar','Email Inválido.')
+        }
+
+        if(error.code === 'auth/user-not-found'){
+          return Alert.alert('Registar','Email ou senha Inválido.')
+        }
+
+        return Alert.alert('Registar', 'Não foi possível acessar.')
+
+      });
+
+
+  }
+
+  return (
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Logo />
+
+        <HStack>
+            <IconButton 
+                icon={<CaretLeft color={colors.gray[200]} size={24}/>}
+            />
+            <Heading color="gray.100" fontSize="xl" mt={20} mb={6} ml={4}>
+                Crie sua conta
+            </Heading>
+        </HStack>
+      
+
+      <Input
+        mb={4}
+        placeholder="E-mail"
+        InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
+        onChangeText={setEmail}
+      />
+
+      <Input
+        mb={8}
+        placeholder="Senha"
+        InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
+        secureTextEntry
+        onChangeText={setPassword}
+      />
+
+     <Input
+        mb={8}
+        placeholder="Confirmar senha"
+        InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
+        secureTextEntry
+        onChangeText={setConfirmPassword}
+      />
+
+      <Button 
+        title="Registar" 
+        w="full"
+        isLoading={isLoading} 
+        onPress={handleSignOut} 
+      />
+    </VStack>
+  )
+}
